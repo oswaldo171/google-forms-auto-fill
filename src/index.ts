3956cf155b06b4b95b45ed93aaff97001b648d9e
Carlos Dubón <holacarlosdubon@gmail.com>
2021-04-17 16:47:18 -0600

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,19 +4,16 @@ const app = async () => {
   const formURL: string =
     "https://docs.google.com/forms/d/e/1FAIpQLSe19oQwJlazDrHxGq4jrHa1ek6htjDY5pyi-wz1sXHLeJrw0A/viewform";
 
-  // Pass this as an object to puppeteer.launch() for debugging
-  //   {
-  //     headless: false,
-  //     slowMo: 10,
-  //     devtools: true,
-  //   }
-
   const student = {
     name: "Dubón Lémus Carlos Daniel",
     id: "201943354",
   };
 
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    headless: false,
+    slowMo: 10,
+    devtools: false,
+  });
 
   const page = await browser.newPage();
 
@@ -44,6 +41,7 @@ const app = async () => {
   debugger;
 
   await browser.close();
+  console.log("Done! Data was sent.");
 };
 
 app();
