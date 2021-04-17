8647e8bd1c9da1eadc2a7a5583505f727ab5ab1a
Carlos Dubón <holacarlosdubon@gmail.com>
2021-04-16 18:48:05 -0600

    Moved to puppeteer

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,30 @@
-import request from "request";
-import cheerio from "cheerio";
+// import request from "request";
+// import cheerio from "cheerio";
+
+// const formURL: string =
+//   "https://docs.google.com/forms/d/e/1FAIpQLSfYroBk6rCP2CbWbqJXhFrYupo25_VeFVEab-gy5_VtJ5fvgA/viewform";
+
+// request(formURL, (error, response, html) => {
+//   if (!error && response.statusCode == 200) {
+//     const $ = cheerio.load(html);
+
+//     let userInputClass: string = ".quantumWizTextinputPaperinputInputArea";
+
+//     $(userInputClass).each((i, el) => {
+//       const element = $(el);
+//       const inputEl = element.children("input");
+//       inputEl.attr("value", "Carlos");
+//     });
+
+//     const sendButton = $(".appsMaterialWizButtonPaperbuttonContent");
+
+//     setTimeout(()=>{
+//         $(".appsMaterialWizButtonPaperbuttonContent").trigger()
+//     },500);
+//   }
+// });
+
+import puppeteer from "puppeteer";
 
 const formURL: string =
   "https://docs.google.com/forms/d/e/1FAIpQLSfYroBk6rCP2CbWbqJXhFrYupo25_VeFVEab-gy5_VtJ5fvgA/viewform";
-
-request(formURL, (error, response, html) => {
-  if (!error && response.statusCode == 200) {
-    const $ = cheerio.load(html);
-  }
-});
